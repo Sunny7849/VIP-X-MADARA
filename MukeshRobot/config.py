989cd35b59733d70434577ac4e0061942e7ee84a
Sunny7849 <164983493+Sunny7849@users.noreply.github.com>
2025-04-20 06:31:33 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -47,7 +47,7 @@ class Config(object):
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("6675050163", "6881964861")
     WOLVES = get_user_list("6675050163", "6881964861")
-    DONATION_LINK = "https://t.me/the_vip_boy" # EG, paypal
+    DONATION_LINK = "https://t.me/VIP_DONATION_BOT" # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
